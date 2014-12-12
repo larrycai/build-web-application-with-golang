83f3c7e132871e1b5af871ca0453a2a3f9fbafc0
Larry Cai <larry.caiyu@gmail.com>
2014-12-12 13:23:42 +0800

    Create .drone.io.sh

---
+++ ebook/.drone.io.sh
@@ -0,0 +1,6 @@
+sudo apt-get install pandoc
+uname -a
+lsb_release -a
+pandoc -v 
+sudo apt-get install ttf-arphic-gbsn00lp ttf-arphic-ukai ttf-wqy-microhei ttf-wqy-zenhei
+sudo apt-get install texlive-xetex texlive-latex-recommended texlive-latex-extra
